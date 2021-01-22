05952a698e778ae7c67687eeb104afd9bba2bb3e
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-01-22 09:10:16 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+# This is a Python Program that finds the table of the Entered Number.
+a = int(input("Enter the Number for which you want to find the table of: "))
+
+print(a, "X", 1, "=", a*1)
+print(a, "X", 2, "=", a*2)
+print(a, "X", 3, "=", a*3)
+print(a, "X", 4, "=", a*4)
+print(a, "X", 5, "=", a*5)
+print(a, "X", 6, "=", a*6)
+print(a, "X", 7, "=", a*7)
+print(a, "X", 8, "=", a*8)
+print(a, "X", 9, "=", a*9)
+print(a, "X", 10, "=", a*10)
